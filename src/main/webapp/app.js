593d3494ce7db39cf300f6ac09a060e904a92143
Breno <brenoman@gmail.com>
2016-05-12 18:22:23 -0300

    Começando a mexer nos aspects da aplicação

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,17 +1,10 @@
 (function() {
   var app = angular.module("teste", []);
-  app.controller("ControllerAlunos", ["$http", function($http) {
-    this.aluno = {};
-	this.addAlunoRest = function() {
-      $http.post('http://localhost:8080/chamadaonline/alunos/salvar', this.aluno);
+  app.controller("ControllerPessoas", ["$http", function($http) {
+    this.pessoa = {};
+	this.addPessoaRest = function() {
+      $http.post('http://localhost:8080/hotelspring/alunos/salvar', this.aluno);
     };
-  
-    this.atualizar = function() {
-    	var that = this;
-    	$http.get('http://localhost:8080/chamadaonline/alunos').success(function(data){
-    		that.db = data;
-    	});
-    };
-    
+	
   }]);
 })();
